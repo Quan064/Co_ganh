be1bebb63bdfc30b8d8a4a851a600755264cba66
Minh Quan <minhquanthong064@gmail.com>
2023-12-16 12:21:27 +0700

    new

--- test_code.py
+++ test_code.py
@@ -1,6 +1,27 @@
-a = [3]
+from PIL import Image, ImageDraw
 
-dict = {1 : a, 2 : 3}
+image = Image.new("RGB", (600, 600), "WHITE")
+draw = ImageDraw.Draw(image)
 
-a[0] = 3
-print(dict)
+draw.line((100, 100, 500, 100), fill="black", width=3)
+draw.line((100, 200, 500, 200), fill="black", width=3)
+draw.line((100, 300, 500, 300), fill="black", width=3)
+draw.line((100, 400, 500, 400), fill="black", width=3)
+draw.line((100, 500, 500, 500), fill="black", width=3)
+draw.line((100, 100, 100, 500), fill="black", width=3)
+
+draw.line((200, 100, 200, 500), fill="black", width=3)
+draw.line((300, 100, 300, 500), fill="black", width=3)
+draw.line((400, 100, 400, 500), fill="black", width=3)
+draw.line((500, 100, 500, 500), fill="black", width=3)
+draw.line((100, 100, 500, 500), fill="black", width=3)
+draw.line((100, 500, 500, 100), fill="black", width=3)
+
+draw.line((100, 300, 300, 100), fill="black", width=3)
+draw.line((300, 100, 500, 300), fill="black", width=3)
+draw.line((500, 300, 300, 500), fill="black", width=3)
+draw.line((300, 500, 100, 300), fill="black", width=3)
+
+image2 = image.copy()
+
+image2.show()
