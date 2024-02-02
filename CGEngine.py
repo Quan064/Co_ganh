17734b3aeced8941f8850272cf472334ad31b985
Minh Quan <minhquanthong064@gmail.com>
2024-02-02 15:54:36 +0700

    new

--- CGEngine.py
+++ CGEngine.py
@@ -3,7 +3,7 @@ import random
 
 # Remember that board[y][x] is the tile at (x, y) when printing
     
-def is_valid_move(move, current_side, board):
+def is_valid_move(move, current_side, board): # HÀM HỖ TRỢ: KIỂM TRA NƯỚC ĐI HỢP LỆ
     current_x = move["selected_pos"][0]
     current_y = move["selected_pos"][1]
     new_x = move["new_pos"][0]
@@ -19,7 +19,7 @@ def is_valid_move(move, current_side, board):
         return (current_x+current_y)%2==0 and (dx * dy == 1)
     return False
 
-def main(player):
+def main(player): # BẮT BUỘC (KHÔNG XÓA)
 
     # {'your_pos': [(0,0), (1,0), (2,0), (3,0), (4,0), (0,1), (4,1), (4,2)],
     #  'your_side': -1,
@@ -40,7 +40,7 @@ def main(player):
             return move
 
 # Function of the game manager
-def random_move(position):
+def random_move(position): # ĐIỀU CHỈNH THUẬT TOÁN TẠI ĐÂY
     movement = [(0, -1), (0, 1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]  #possible moves
     movement_select = random.choice(movement)  #Randomize movement
     new_pos_x = position[0] + movement_select[1]
