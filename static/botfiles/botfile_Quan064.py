1231cdde551ced380bcc89d7149f7931235ae273
Minh Quan <minhquanthong064@gmail.com>
2024-02-18 23:16:17 +0700

    new

--- static/botfiles/botfile_Quan064.py
+++ static/botfiles/botfile_Quan064.py
@@ -1,9 +1,7 @@
 
 import random
 
-# Remember that board[y][x] is the tile at (x, y) when printing
-    
-def is_valid_move(move, current_side, board): # HÀM HỖ TRỢ: KIỂM TRA NƯỚC ĐI HỢP LỆ
+def is_valid_move(move, current_side, board):
     current_x = move["selected_pos"][0]
     current_y = move["selected_pos"][1]
     new_x = move["new_pos"][0]
@@ -19,16 +17,7 @@ def is_valid_move(move, current_side, board): # HÀM HỖ TRỢ: KIỂM TRA NƯ�
         return (current_x+current_y)%2==0 and (dx * dy == 1)
     return False
 
-def main(player): # BẮT BUỘC (KHÔNG XÓA)
-
-    # {'your_pos': [(0,0), (1,0), (2,0), (3,0), (4,0), (0,1), (4,1), (4,2)],
-    #  'your_side': -1,
-    #  'opp_pos': [(0,0), (1,0), (2,0), (3,0), (4,0), (0,1), (4,1), (4,2)],
-    #  'board': [[-1,-1, 0,-1, 0],
-    #            [ 0,-1,-1,-1, 0],
-    #            [-1, 0, 0,-1, 1],
-    #            [ 0, 1, 1, 1, 1],
-    #            [ 1, 1, 0, 1, 0]]}
+def main(player):
 
     while True:
         selected_pos = random.choice(player.your_pos)
@@ -37,10 +26,9 @@ def main(player): # BẮT BUỘC (KHÔNG XÓA)
         new_pos = (new_pos_select[0], new_pos_select[1])
         move = {"selected_pos": selected_pos, "new_pos": new_pos}
         if is_valid_move(move, player.your_side, board):
-            return move
+            return None
 
-# Function of the game manager
-def random_move(position): # ĐIỀU CHỈNH THUẬT TOÁN TẠI ĐÂY
+def random_move(position):
     movement = [(0, -1), (0, 1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]  #possible moves
     movement_select = random.choice(movement)  #Randomize movement
     new_pos_x = position[0] + movement_select[1]
