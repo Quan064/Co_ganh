3d32a64e250c76fe9fc17bba4202dc0fa03d6135
letuanvu <ngovipmo1@gmail.com>
2024-02-24 20:23:50 +0700

    hoan thien trang code web

--- static/botfiles/botfile_tlv23.py
+++ static/botfiles/botfile_tlv23.py
@@ -1,8 +1,6 @@
 
 import random
 
-# Remember that board[y][x] is the tile at (x, y) when printing
-    
 def is_valid_move(move, current_side, board):
     current_x = move["selected_pos"][0]
     current_y = move["selected_pos"][1]
@@ -21,17 +19,8 @@ def is_valid_move(move, current_side, board):
 
 def main(player):
 
-    # {'your_pos': [(0,0), (1,0), (2,0), (3,0), (4,0), (0,1), (4,1), (4,2)],
-    #  'your_side': -1,
-    #  'opp_pos': [(0,0), (1,0), (2,0), (3,0), (4,0), (0,1), (4,1), (4,2)],
-    #  'board': [[-1,-1, 0,-1, 0],
-    #            [ 0,-1,-1,-1, 0],
-    #            [-1, 0, 0,-1, 1],
-    #            [ 0, 1, 1, 1, 1],
-    #            [ 1, 1, 0, 1, 0]]}
-
     while True:
-        selected_pos = random.choice(player.your_pos)
+        selected_pos = random.choice(player.)
         board = player.board
         new_pos_select = random_move(selected_pos)
         new_pos = (new_pos_select[0], new_pos_select[1])
@@ -39,7 +28,6 @@ def main(player):
         if is_valid_move(move, player.your_side, board):
             return move
 
-# Function of the game manager
 def random_move(position):
     movement = [(0, -1), (0, 1), (1, 0), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]  #possible moves
     movement_select = random.choice(movement)  #Randomize movement
