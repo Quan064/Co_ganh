93594a88738e8acb8fd3da0f6911f254c86fe9a4
Quan064 <minhquanthong064@gmail.com>
2024-06-04 16:32:00 +0700

    fix bug level 4

--- trainAI/level2.py
+++ trainAI/level2.py
@@ -27,8 +27,9 @@ def minimax(your_pos, opp_pos, your_board, opp_board):
                 if len(your_new_pos) > len(opp_new_pos) or len(your_new_pos) == len(opp_new_pos) == 3:
                     opp_new_board, opp_new_pos = vay(opp_new_pos, your_new_board, opp_new_board)
 
-                value = (len(your_new_pos) - len(opp_new_pos)), check_pos_point(your_new_board, opp_new_board)
+                value = (len(your_new_pos) + len(opp_new_pos)), check_pos_point(your_new_board, opp_new_board)
 
+                print(value)
                 if value > bestVal:
                     move["selected_pos"] = pos
                     move["new_pos"] = invalid_move
